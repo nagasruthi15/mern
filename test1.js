f73c23a4260047a566a10c25a94fbe48d58889e8
nagasruthi9966185195@gmail.com <nagasruthi9966185195@gmail.com>
2024-09-26 10:50:54 +0530

    secode added

--- test1.js
+++ test1.js
@@ -1,1 +1,14 @@
-console.log("welcome to nodejs")
+function Test(){
+    let counter=0;
+    function updateCount(){        
+        counter++;
+        console.log(counter);        
+    }
+    return(
+        <div>
+            <h2>Counter value is{counter}</h2>
+<button onClick={updateCount}>change</button>
+        </div>
+    )
+}
+export default Test;
